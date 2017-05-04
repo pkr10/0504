02d6b987220cd4c9638d45639d9a81dcf80b5735
박경록 <pkr10@naver.com>
2017-05-04 16:52:52 +0900

    초안

--- app/src/main/java/com/example/rok/a0504/MainActivity.java
+++ app/src/main/java/com/example/rok/a0504/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,16 +20,24 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     EditText e1;
     WebView w1;
     Animation a1;
-    LinearLayout l1;
-
+    LinearLayout l1,l2;
+    ListView listview;
+    ArrayList<String> data= new ArrayList<String>();
+    ArrayList<String> data1 = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +45,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         e1 = (EditText)findViewById(R.id.editText);
         w1 = (WebView)findViewById(R.id.webview);
-        w1.addJavascriptInterface(new Javascriptmethod(),"MyApp");
+        w1.addJavascriptInterface(new siteAdd(),"MyApp");
         l1 = (LinearLayout)findViewById(R.id.linear);
+        l2 = (LinearLayout)findViewById(R.id.linear2);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        listview = (ListView)findViewById(R.id.list);
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                l2.setVisibility(View.VISIBLE);
+                l1.setVisibility(View.VISIBLE);
+                listview.setVisibility(View.INVISIBLE);
+                w1.loadUrl("http://"+data1.get(position).toString());
+            }
+        });
+        Log.d("데이터",data1.toString());
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("삭제확인").setMessage("정말이에요").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        data.remove(data.get(position));
+                        data1.remove(data1.get(position));
+                        adapter.notifyDataSetChanged();
+                    }
+                }).setNegativeButton("NO",null).show();
+
+                return false;
+            }
+        });
         final ProgressDialog dialog;
         dialog = new ProgressDialog(this);
         w1.setWebViewClient(new WebViewClient(){
@@ -86,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                l1.setVisibility(View.GONE);
+                l2.setVisibility(View.GONE);
 
             }
 
@@ -103,50 +142,80 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.add(0,1,0,"즐겨찾기");
+        menu.add(0,2,0,"즐겨찾기 추가");
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId()==1){
-            w1.loadUrl("file:///android_asset/www/index.html");
             l1.setAnimation(a1);
+            l1.setVisibility(View.INVISIBLE);
 
             a1.start();
+            listview.setVisibility(View.VISIBLE);
         }
+        if(item.getItemId()==2){
+            w1.loadUrl("file:///android_asset/www/urladd.html");
+            l2.setAnimation(a1);
+            a1.start();
+    }
         return super.onOptionsItemSelected(item);
     }
     Handler handler = new Handler();
 
-    class Javascriptmethod{
+//    class Javascriptmethod{
+//        @JavascriptInterface//이렇게 해준 애만 웹페이지에 호출가능
+//        public void displayToast(){
+//            Toast.makeText(getApplicationContext(),"추가되었습니다",Toast.LENGTH_SHORT).show();
+//            handler.post(new Runnable() {
+//                @Override
+//                public void run() {
+//                    AlertDialog.Builder builder =  new AlertDialog.Builder(MainActivity.this);
+//                    builder.setTitle("그림변경").setMessage("그림을 변경하시겠습니까").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            w1.loadUrl("javascript:changeImage()");
+//                        }
+//                    }).setNegativeButton("cancel",null).show();
+//                }
+//            });
 
-        @JavascriptInterface//이렇게 해준 애만 웹페이지에 호출가능
-        public void displayToast(){
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    AlertDialog.Builder builder =  new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("그림변경").setMessage("그림을 변경하시겠습니까").setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            w1.loadUrl("javascript:changeImage()");
-                        }
-                    }).setNegativeButton("cancel",null).show();
-                }
-            });
 
+//        }
+//        public void getdata(String value){
+//            data.add(value);
+//            adapter.notifyDataSetChanged();
+//        }
 
+//    }
+    class siteAdd {
+        @JavascriptInterface
+        public void getdata(String value,String value1) {
+            data data2 = new data(value,value1);
+            data.add(data2.name);
+            data1.add(data2.url);
+            adapter.notifyDataSetChanged();
         }
+    @JavascriptInterface
+    public void back(){
+        l2.setVisibility(View.VISIBLE);
+    }
+
+
+
 
     }
 
+
     public void onmyclick(View view) {
         switch(view.getId()){
-            case R.id.button2:
-                w1.loadUrl("javascript:changeImage()");
+            case R.id.button:
+                w1.loadUrl("http://"+e1.getText().toString());
                 break;
         }
     }
---
+++ app/src/main/java/com/example/rok/a0504/data.java
@@ -0,0 +1,14 @@
+package com.example.rok.a0504;
+
+/**
+ * Created by rok on 2017. 5. 4..
+ */
+
+public class data {
+    String name;
+    String url;
+    public data(String name,String url){
+        this.name = name;
+        this.url = url;
+    }
+}
